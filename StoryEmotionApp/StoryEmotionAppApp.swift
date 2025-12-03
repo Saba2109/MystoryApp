81b53fdf2363d1ca15a5655c337ea2ab76c4a2ff
Saba2109 <sabarasheed998@gmail.com>
2025-12-03 10:31:45 +0300

    Initial Commit

---
+++ StoryEmotionApp/StoryEmotionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryEmotionAppApp.swift
+//  StoryEmotionApp
+//
+//  Created by saba alrasheed on 12/06/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct StoryEmotionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoryEmotionAppTests/StoryEmotionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryEmotionAppTests.swift
+//  StoryEmotionAppTests
+//
+//  Created by saba alrasheed on 12/06/1447 AH.
+//
+
+import Testing
+@testable import StoryEmotionApp
+
+struct StoryEmotionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
